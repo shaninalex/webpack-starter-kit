b5df70a8db08cf9882afdd1194dc9c2d0f911eb5
alex-digisol <alex@digisol.agency>
2021-08-02 21:35:05 +0300

    screen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,25 @@
 import '../scss/style.scss';
 
-console.log('initialized')
+// import { tns } from "tiny-slider";
+
+// console.log('initialized')
+
+const toggle_btns = document.querySelectorAll('[toggle-menu]');
+const menu = document.getElementById('main-menu');
+
+[].forEach.call(toggle_btns, btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn);
+        btn.classList.toggle('show');
+        menu.classList.toggle('show');
+    });
+});
+
+
+
+var slider = tns({
+    container: '.gallery',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false
+});
